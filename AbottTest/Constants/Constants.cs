ecba1ad9996d25923bd70fe97a68e5607dee4946
Raj123456788 <giriraj.kulkarni@gmail.com>
2023-06-05 17:54:33 -0700

    Update Constants.cs

--- AbottTest/Constants/Constants.cs
+++ AbottTest/Constants/Constants.cs
@@ -6,8 +6,8 @@ namespace AbottTest.Constants
 {
     public static class Constants
     {        
-        public const string passwordLibView = "P@ssword$12";
-        public const string username = "codechallengeadc@outlook.com";
-        public const string passwordOutlook = "P@ssword$1234";        
+        public const string passwordLibView = "";
+        public const string username = "";
+        public const string passwordOutlook = "";        
     }
 }
